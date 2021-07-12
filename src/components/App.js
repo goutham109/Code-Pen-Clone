bfeb849e2323e7e2317156aa3c22975d40d09d7c
Goutham232 <katpallygoutham123@gmail.com>
2021-07-12 13:31:57 +0530

    Built the App

---
+++ src/components/App.js
@@ -0,0 +1,64 @@
+import React,{ useState,useEffect } from 'react';
+import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+function App() {
+
+  const [html,setHtml]      = useLocalStorage('html','');
+  const [css,setCSS]        = useLocalStorage('css','');
+  const [js,setJS]          = useLocalStorage('js','');
+  const [srcDoc,setSrcDoc]  = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `)
+    },500)
+    return () => clearTimeout(timeout)
+  },[html,css,js])
+
+  return (
+    <>
+      <div className = "pane top-pane">
+        <Editor 
+          language    = "xml"
+          displayName = "HTML"
+          value       = {html}
+          onChange    = {setHtml}
+        />
+
+        <Editor 
+          language    = "css"
+          displayName = "CSS"
+          value       = {css}
+          onChange    = {setCSS}
+        />
+
+        <Editor 
+          language    = "javascript"
+          displayName = "JS"
+          value       = {js}
+          onChange    = {setJS}
+        />
+      </div>
+
+      <div className="pane">
+        <iframe
+          title="output"
+          srcDoc={srcDoc}
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width = "100%"
+          height = "100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
